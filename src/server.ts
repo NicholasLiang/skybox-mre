ab639b93ede869b3e97412a8e237d9d3656de3de
Nicholas Liang <ssss021418@gmail.com>
2021-09-08 21:34:10 -0700

    update project to use paramaters

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
-import { Appearance } from '@microsoft/mixed-reality-extension-sdk';
 import dotenv from 'dotenv';
 import { resolve as resolvePath } from 'path';
 // add some generic error handlers here, to log any exceptions we're not expecting
@@ -7,9 +6,6 @@ process.on('uncaughtException', err => console.log('uncaughtException', err));
 process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
 // Read .env if file exists
 dotenv.config();
-
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-
 
 interface uri {
     id: string;
@@ -20,21 +16,22 @@ interface uris {
     ids: uri[];
 }
 
-var urisDatabase: uris = require('../public/test.json');
+const wrap = (s: string, w: number) => s.replace(
+    new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, 'g'), '$1\n'
+);
 
 class Skybox {
     private assets: MRE.AssetContainer;
     private skybox: MRE.Actor = null;
     private mat: MRE.Material;
+    private errorText: MRE.Actor;
 
     constructor(private context: MRE.Context, private params: MRE.ParameterSet, private baseUrl: string) {
-        this.context.onStarted(() => this.started());
+        this.context.onStarted(() => this.started(params));
     }
 
-    private async started() {
+    private async started(params: MRE.ParameterSet) {
         this.assets = new MRE.AssetContainer(this.context);
-        
-        
         const skyboxData = await this.assets.loadGltf('skyboxSphere.glb', 'box');
         this.mat = this.assets.materials[0];
         
@@ -49,22 +46,19 @@ class Skybox {
             }
         });
 
-        
-        if (urisDatabase.ids.find(x => x.id == this.context.sessionId))
-        {
-            var index = urisDatabase.ids.findIndex(x => x.id == this.context.sessionId)
-            const tex = this.assets.createTexture('uvgrid', {
-                uri: urisDatabase.ids[index].uri
-            });
-            this.mat.emissiveTexture = tex;
-        }
-        else
-        {
-            const tex = this.assets.createTexture('uvgrid', {
-                uri: `${this.baseUrl}/Gy09v.jpg`
-            });
-            this.mat.emissiveTexture = tex;
-        }
+        this.errorText = MRE.Actor.Create(this.context, {
+            actor: {
+                text: {
+                    contents: "Error",
+                    height: 0.1,
+                    anchor: MRE.TextAnchorLocation.MiddleCenter,
+                    justify: MRE.TextJustify.Center,
+                    color: MRE.Color3.Red(),
+                }
+            }
+        })
+
+        this.errorText.text.enabled = false;
 
         const textButton = MRE.Actor.Create(this.context, {
             actor: {
@@ -85,11 +79,6 @@ class Skybox {
                     // textButton.text.contents =
                     //     `Click for prompt\nLast response: ${res.submitted ? res.text : "<cancelled>"}`;
 
-                    var index = urisDatabase.ids.findIndex(x => x.id == this.context.sessionId);
-                    if (index > -1) {
-                        urisDatabase.ids.splice(index, 1);
-                    }
-
                     this.reloadImage(res.text);
                 })
                 .catch(err => {
@@ -97,23 +86,23 @@ class Skybox {
                 });
         });
 
+        if (params.url != null) {
+            this.reloadImage(params.url);
+        } else {
+            this.reloadImage(`${this.baseUrl}/Gy09v.jpg`);
+        }
+
         return true;
     }
 
-
-    private reloadImage(url: string) {
+    private reloadImage(url: any) 
+    {
         const tex = this.assets.createTexture(`${url}`, {
             uri: url
         });
-
         this.mat.emissiveTexture = tex;
-
-        const tmp:uri = {id: this.context.sessionId, uri: url};
-        urisDatabase.ids.push(tmp);
     }
 }
-
-
 
 function runApp() {
     // Start listening for connections, and serve static files.
